eac93df7f81e37d2e5c76bb6c7b2629dc3185aaa
jesstepa <35516875+jesstepa@users.noreply.github.com>
2021-04-05 00:40:28 -0400

    stuff

--- view.js
+++ view.js
@@ -1,4 +1,45 @@
 // ---- Define your dialogs  and panels here ----
+// STUDIO ---------------------------
+var effective_permissions_panel = define_new_effective_permissions('e_p_panel', true, null)
+$('#sidepanel').append(effective_permissions_panel)
+
+var user_select_field = define_new_user_select_field('u_s_field', 'select_button_text', function(selected_user){
+    // HARDCODED FILEPATH (can probably change by figuring out which element is associated with the locks
+    // and then doing the attr thing to set the filepath to whatever the lock's filepath is)
+    $('#e_p_panel').attr('filepath', '/C/Lecture_Notes/Lecture1.txt')
+    $('#e_p_panel').attr('username', selected_user)})
+$('#sidepanel').prepend(user_select_field)
+
+var new_dialog = define_new_dialog('n_dialog', title='', options = {})
+$('.perm_info').click(function(){
+    console.log('clicked!')
+    new_dialog.dialog('open')
+    new_dialog.empty()
+
+    var filepath = $('#e_p_panel').attr('filepath')
+    var username = $('#e_p_panel').attr('username')
+    var perm_name = $(this)
+    console.log(filepath)
+    console.log(username)
+    console.log(perm_name)
+
+    var file_object = path_to_file[filepath]
+    var user_object = all_users[username]
+    var is_user_action_allowed = allow_user_action(file_object, user_object, perm_name, true)
+    var readable_is_user_action_allowed = get_explanation_text(is_user_action_allowed)
+
+    new_dialog.append(username)
+    new_dialog.append("<br>")
+    new_dialog.append("<br>")
+    new_dialog.append(filepath)
+    new_dialog.append("<br>")
+    new_dialog.append("<br>")
+    new_dialog.append(readable_is_user_action_allowed)
+})
+
+// allow_user_action(file, user, permission_to_check, explain_why = false)
+
+
 
 
 
